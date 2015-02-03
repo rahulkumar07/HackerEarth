b1f7d4b3e346d8d899ff1bf8b146f121bc9d927e
Rahul Kumar <rahulkaitian@gmail.com>
2015-02-03 14:13:02 +0530

    Create TestClass7.java

---
+++ TestClass7.java
@@ -0,0 +1,33 @@
+    import java.io.BufferedReader;
+    import java.io.InputStreamReader;
+    
+    import java.util.ArrayList;
+
+/*Solution to hackerEarth of the Problem"Alien language"
+ * 
+ * URL: http://www.hackerearth.com/problem/algorithm/fizzbuzz/
+ Problem statement:
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+For numbers which are multiples of both three and five print “FizzBuzz”.
+Print a new line after each string or number.
+ */
+class TestClass7 {
+    public static void main(String args[] ) throws Exception {
+        /*
+         * Read input from stdin and provide input before running
+*/
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] lineValues = br.readLine().split("");
+        int N = Integer.parseInt(line);
+        for (int i = 0; i < N; i++) {
+            System.out.println("hello world");
+        }
+        
+
+        System.out.println("Hello World!");
+    }
+}
+
+
+   
